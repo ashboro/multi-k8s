5e2ad3ec6bbe64fe72f5cc05f0053793638ed88c
Ashley Marron <borostreets@outlook.com>
2020-08-12 19:26:31 +0100

    101 ashboro Skaffold plus SSL

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 docker build -t ashboro/multi-client:latest -t ashboro/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t ashboro/multi-server:latest -t ashboro/multi-server:$SHA   -f ./client/Dockerfile ./server
-docker build -t ashboro/multi-worker:latest -t ashboro/multi-worker:$SHA -f ./client/Dockerfile ./worker
+docker build -t ashboro/multi-server:latest -t ashboro/multi-server:$SHA   -f ./server/Dockerfile ./server
+docker build -t ashboro/multi-worker:latest -t ashboro/multi-worker:$SHA -f ./woker/Dockerfile ./worker
 docker push ashboro/multi-client:latest
 docker push ashboro/multi-server:latest
 docker push ashboro/multi-worker:latest
 docker push ashboro/multi-client:$SHA
 docker push ashboro/multi-server:$SHA
 docker push ashboro/multi-worker:$SHA
-
 kubectl apply -f k8s
 kubectl set image deployments/sever-deployment server=ashboro/multi-server:$SHA
 kubectl set image deployments/client-deployment client=ashboro/multi-client:$SHA
